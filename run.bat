edee8137ec7c6e2245c2adc86b1b643ed8796a9c
renatormc <32611729+renatormc@users.noreply.github.com>
2020-10-11 18:53:47 -0300

    Delete run.bat

--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set PATH=%PATH%;"%SINFTOOLS%\extras\iped\iped-3.17-snapshot\tools\tsk\x64"
-mvn compile exec:java ^
--Dexec.mainClass="go.sptc.sinf.App" ^
--Dexec.workingdirectory="H:\grupo_geccor\B10\processamento" ^
--Dexec.args="-c H:\grupo_geccor\B10\processamento -d D:\temp\deletar -q H:\grupo_geccor\B10\processamento\query.txt -l 50" ^
--Dexec.classpathScope=runtime 
